3113aa9eb3b8e5bd1542e4a9dc2de274915e4126
yuinab b <yuinacats@gmail.com>
2024-05-03 04:02:43 +0000

    fixed bugs

--- my-import-script.py
+++ my-import-script.py
@@ -10,35 +10,20 @@ db = client.ncd6fc
 # specify a collection
 collection = db.dp2
 path = "data"
-complete_imported = 0
-complete_non_imported = 0
-corrupted = 0
 
-
-for (root, dirs, files) in os.walk(path):
-    for f in files:
-        file_path = os.path.join(root, f)
+for filename in os.listdir(path):
+  with open(os.path.join(path, filename)) as f:
+    try:
+        file_data = json.load(f)
+    except Exception as e:
+        print(e, "error when loading", f)
+    if isinstance(file_data, list):
         try:
-            with open(file_path, 'r') as file:
-                file_data = json.load(file)
-                if isinstance(file_data, list):
-                    try:
-                        complete_imported += len(file_data)
-                        collection.insert_many(file_data)
-                        
-                    except Exception as e:
-                        complete_non_imported += 1
-                        print(e, "when importing into Mongo")
-                else:
-                    try:
-                        complete_imported += 1
-                        collection.insert_one(file_data)
-                    except Exception as e:
-                        complete_non_imported += 1
-                        print(e)
+            collection.insert_many(file_data)  
         except Exception as e:
-            print(e, "error when loading", f)
-            corrupted += 1
-print("complete imported", complete_imported)
-print("complete non imported:", complete_non_imported)
-print("corrupted:", corrupted)
+            print(e, "when importing into Mongo")
+    else:
+        try:
+            collection.insert_one(file_data)
+        except Exception as e:
+            print(e)
